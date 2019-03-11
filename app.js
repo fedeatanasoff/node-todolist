4359f54cf94974170269d4391ead0b54f39877c1
fedeatanasoff <fede.atanasoff@gmail.com>
2019-03-11 02:28:36 -0300

    preparando los modulos

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-const singleThread = require("./my_modules/single-thread.js");
-
-singleThread();
+// const singleThread = require("./my_modules/single-thread.js");
+// singleThread();
+const color = require("colors");
+const todoCli = require("./my_modules/todo-commands").argv;
+const todoList = require("./my_modules/todo-list");
--- my_modules/single-thread.js
+++ my_modules/single-thread.js
@@ -1,5 +1,22 @@
-const singleThread = () => {};
+const singleThread = () => {
+  //   process.argv[2] = "agregando otro argv";
+  console.log(`${process.argv}`);
 
-module.exports = {
-  singleThread
+  console.log(
+    `==========================================================
+================= EL PROCESO EN NODE =====================
+ID del proceso: .............. ${process.pid}
+Titulo: ...................... ${process.title}
+Directorio de Node: .......... ${process.execPath}
+Directorio actual: ........... ${process.cwd()}
+Version de Node: ............. ${process.version}
+Versiones Dependencias: ...... ${process.versions}
+Plataforma (S.O): ............ ${process.platform}
+Arquitectura (S.O): .......... ${process.arch}
+Tiempo activo de node.js: .... ${process.uptime()}
+Argumentos del proceso: ...... ${process.argv}
+=========================================================`
+  );
 };
+
+module.exports = singleThread;
